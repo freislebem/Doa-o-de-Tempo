1edcc2e7a05c91a0f4db8976f4752741acaa03aa
Denis Freislebem <si.denis@gmail.com>
2019-01-07 09:39:21 -0200

    Add project files.

---
+++ Doação de Tempo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Doação_de_Tempo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
